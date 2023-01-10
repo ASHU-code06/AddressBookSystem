3e741f4e1f0db3021ce15153178a9e7888689bcc
Ashuhauhan <ashuc306@gmail.com>
2023-01-10 15:23:23 +0600

    AddressBook

---
+++ src/com/bridgelabz/ContactModel.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class ContactModel {
+    public int contactId;
+     public String firstname;
+    public String lastname;
+    public String address;
+    public String city;
+    public String state;
+    public String zip;
+    public String email;
+    public String phoneNumber;
+}
+
--- src/com/bridgelabz/Contacts.java
+++ src/com/bridgelabz/Contacts.java
@@ -1,94 +1,93 @@
 package com.bridgelabz;
-
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 class Contacts {
 
-    private String firstname;
-    private String lastname;
-    private String address;
-    private String city;
-    private String state;
-    private String zip;
-    private String phoneNumber;
-    private String email;
+    ArrayList<ContactModel> myContactlist = new ArrayList<ContactModel>();
+    public int contactId;
+    public void addContact(){//Method for cresting contacts only
+        ContactModel contactModel = new ContactModel();
+        System.out.println("Enter your details as follows ");
+        Scanner myinput = new Scanner(System.in);
 
-    public String getFirstname() {
-        return firstname;
+        System.out.println("Enter first  name ");
+        contactModel.firstname = myinput.nextLine();
+
+        System.out.println("Now enter last name ");
+        contactModel.lastname = myinput.nextLine();
+
+        System.out.println("Enter your address ");
+        contactModel.address = myinput.nextLine();
+
+        System.out.println("Enter city ");
+        contactModel.city = myinput.nextLine();
+
+        System.out.println("Enter state ");
+        contactModel.state = myinput.nextLine();
+
+        System.out.println("Enter ZIP  ");
+        contactModel.zip = myinput.nextLine();
+
+        System.out.println("Enter Phone Number ");
+        contactModel.phoneNumber = myinput.nextLine();
+
+        System.out.println("Enter E-mail ");
+        contactModel.email = myinput.nextLine();
+        
+        myContactlist.add(contactModel);
+    }
+    public void displayContactList(){
+        myContactlist.forEach((contact)->{
+            System.out.println("Name :  "+contact.firstname+" "+contact.lastname);
+            System.out.println("Address :  "+contact.address);
+            System.out.println("City :  "+contact.city);
+            System.out.println("State :  "+contact.state);
+            System.out.println("ZIP :  "+contact.zip);
+            System.out.println("Phone no :  "+contact.phoneNumber);
+            System.out.println("Email  :  "+contact.email);
+        });
     }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
+    public void editContact(){
+        System.out.println("SELECT ");
+        int input = (new Scanner(System.in)).nextInt();
+        System.out.println("1. firstname \n2. lastname \n3. address \n4. city \n5. state \n6. zip \n7. phone-number \n8. email \n9. exit ");
+        int select = (new Scanner(System.in)).nextInt();
+        switch (select) {
+            case 1:
+
+                editContact();
+                break;
+            case 2:
+                editContact();
+                break;
+            case 3:
+                editContact();
+                break;
+            case 4:
+                editContact();
+                break;
+            case 5:
+                editContact();
+                break;
+            case 6:
+                editContact();
+                break;
+            case 7:
+                editContact();
+                break;
+            case 8:
+                editContact();
+                break;
+            case 9:
+                break;
+            default:
+                editContact();
+                break;
+        }
+
+        }
     }
 
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZIP() {
-        return zip;
-    }
-
-    public void setZIP(String ZIP) {
-        this.zip = zip;//if parameter is same as variable that is why this,  is used here
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Contacts(String firstname, String l, String addre, String c, String s, String z, String ph, String e) {
-        this.firstname = firstname;
-        this.lastname = l;
-        this.address = addre;
-        this.city = c;
-        this.state = s;
-        this.zip = z;
-        this.phoneNumber = ph;
-        this.email = e;
-    }
-
-    public String toString(){//here we are aceesing private variables
-        return "First Name: "+firstname+"\nLast Name: "+lastname +"\nAddress:  "+address+"\nCity:  "+city+"\nState:  "+state+"\nZIP:  "+zip+"\nPh.no.:  "+phoneNumber+"\nEmail: "+email;
-            //   \n next line
-    }
-}
